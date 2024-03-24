cca38971df7524e8e98950069d4964db5cc02f53
Rizwan Ali <160789107+Rizwan114@users.noreply.github.com>
2024-03-25 01:21:21 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt([
+    {message:"Enter first number",
+    type:"number",
+    name:"firstNumber",
+},
+{
+    message:"Enter second number",
+    type:"number",
+    name:"secondNumber",
+},
+{
+    message:"Please select of one operator",
+    type:"list",
+    name:"operator",
+    choices:["Addition", "Suptraction", "Multiplication", "Division",],
+},
+]);
+
+// condition statement
+
+if (answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Suptraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);    
+}else if(answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}else {
+    console.log("Please Slect Correct Number");
+    
+}
